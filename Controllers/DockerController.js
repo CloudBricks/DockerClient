ecd2809a6f9555404e8ed5d69e0aaadd25cf47d8
sanjayyepuri <sanjay.yepuri@gmail.com>
2017-08-05 00:06:47 -0500

    Use Container Model

--- Controllers/DockerController.js
+++ Controllers/DockerController.js
@@ -1,9 +1,9 @@
 const Docker = require("dockerode");
-
+const Block = require("../Models/Block");
 
 function DockerController() {
     this.docker = new Docker();
-    this.blocks = { test: 'test' };
+    this.blocks = {};
 }
 DockerController.prototype.createBlock = function(options) {
     const self = this;
@@ -18,7 +18,7 @@ DockerController.prototype.createBlock = function(options) {
             return container.start();
         })
         .then(function(container) {
-            self.blocks[options.name] = container.id;
+            self.blocks[options.name] = new Block(options.Image, container.id);
             console.log(`Block ${options.name} successfully started.`);
         })
 }
@@ -26,7 +26,7 @@ DockerController.prototype.createBlock = function(options) {
 DockerController.prototype.stopBlock = function(options) {
     const self = this;
 
-    const container = this.docker.getContainer(self.blocks[options.name]);
+    const container = this.docker.getContainer(self.blocks[options.name].id);
     console.log(`Block ${options.name} is being removed.`);
     return container.stop()
         .then(function(container) {
